d54132f501c1e0faa36e0d5ace2d73cf5cb053e9
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-27 18:41:55 +0530

    my first program in java

---
+++ cspp2- practice/m1/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * @author SupriyaMadupathi.
+ */
+public final class HelloWorld {
+   /**
+    * empty constructor.
+    */
+    private HelloWorld() {
+
+    }
+   /**
+    * main function to print hello.
+    * @param args is parameter for this function
+    */
+    public static void main(final String[] args) {
+        System.out.println("Hello !!");
+    }
+}
