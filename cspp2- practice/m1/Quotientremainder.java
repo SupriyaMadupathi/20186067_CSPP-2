6053f4bb9a63c9e3efa99ef7bee1de16446d9cb5
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-27 19:29:47 +0530

    quotientremainder

---
+++ cspp2- practice/m1/Quotientremainder.java
@@ -0,0 +1,23 @@
+/**
+ * @author SupriyaMadupathi.
+ */
+public final class Quotientremainder {
+   /**
+    * empty constructor.
+    */
+    private Quotientremainder() {
+
+    }
+   /**
+    * main function to print hello.
+    * @param args is parameter for this function
+    */
+    public static void main(final String[] args) {
+        int a = 8;
+        int b = 6;
+        int c = a / b;
+        int d = a % b;
+
+        System.out.println(c + " " + d);
+    }
+}
