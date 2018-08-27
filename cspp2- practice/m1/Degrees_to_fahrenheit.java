3d3a3a0a7029efff2c6a7e99cc281167cf1d4fd3
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-27 19:47:45 +0530

    converting degree to fahrenheit

---
+++ cspp2- practice/m1/Degrees_to_fahrenheit.java
@@ -0,0 +1,21 @@
+/**
+ * @author SupriyaMadupathi.
+ */
+public final class Degrees_to_fahrenheit {
+   /**
+    * empty constructor.
+    */
+    private Degrees_to_fahrenheit() {
+
+    }
+   /**
+    * main function to print hello.
+    * @param args is parameter for this function
+    */
+    public static void main(final String[] args) {
+        int t = 8;
+        double f = ((t * 1.8) + 32);
+
+        System.out.println(f);
+    }
+}
