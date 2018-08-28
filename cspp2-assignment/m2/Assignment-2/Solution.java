7f4a3bb23cb9b8e0d05bb8fbb184f51cefa89387
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-28 17:11:39 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignment/m2/Assignment-2/Solution.java
+++ cspp2-assignment/m2/Assignment-2/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
 	/**
 	 *do not modify this main function.
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		/**
 		 * function to read input from user
 		 */
