28149f93ac5f5e1df847a9e07f0cce21c27776d8
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-28 17:15:23 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignment/m2/Assignment-2/Solution.java
+++ cspp2-assignment/m2/Assignment-2/Solution.java
@@ -1,34 +1,35 @@
 import java.util.Scanner;
-
+/**
+ *do not modify this main function.
+ * @author Supriya
+ */
 public class Solution {
 
-	private static final int FOUR = 4;
-	/**
-	 *do not modify this main function.
-	 */
-	public static void main(final String[] args) {
-		/**
-		 * function to read input from user
-		 */
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
+    private static final int FOUR = 4;
+    /**
+     *do not modify this main function.
+     *@param args
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
 
 
+    /**
+     *Need to write the rootsOfQuadraticEquation function and print  output.
+     *@param a
+     *@param b
+     *@param c
+     */
     public static void rootsOfQuadraticEquation(int a, int b, int c) {
-    	/**
-    	 *main function to calculate roots
-    	 */
-		long det = (b * b) - (FOUR * a * c);
-		double root1 = (-b + Math.sqrt(det)) / (2 * a);
-		double	root2 = (-b - Math.sqrt(det)) / (2 * a);
-		System.out.println(root1 + " " + root2);
-		}
-	}
-	/**
-	 *Need to write the rootsOfQuadraticEquation function and print  output.
-	 */
+        long det = (b * b) - (FOUR * a * c);
+        double root1 = (-b + Math.sqrt(det)) / (2 * a);
+        double  root2 = (-b - Math.sqrt(det)) / (2 * a);
+        System.out.println(root1 + " " + root2);
+        }
+    }
 
