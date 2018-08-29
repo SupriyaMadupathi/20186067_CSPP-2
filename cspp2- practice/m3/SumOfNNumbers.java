9e9408641164a19a7f9341227492b944e390831c
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-29 14:32:25 +0530

    sum of n numbers

--- cspp2- practice/m3/SumOfNNumbers.java
+++ cspp2- practice/m3/SumOfNNumbers.java
@@ -2,23 +2,18 @@ import java.util.Scanner;
 /**
  * @author     SupriyaMadupathi
  */
-public final class Sum_Of_N_Numbers {
-  
-    private Sum_Of_N_Numbers() {
-        //Iam not using this constructor
+public final class SumOfNNumbers {
+    /**.
+     * Constructs the object.
+     */
+    private SumOfNNumbers() {
     }
     /**
     * Main function to print.
     * @param args is the parameter for this function
     */
-    private static final int FOUR = 4;
 
-    /**
-     * { function_description }
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
+   public static void main(final String[] args) {
         int n, i = 1;
         int sum = 0;
         System.out.println("Enter the Number :");
