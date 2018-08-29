3a66a05d400134ba5db59465192963d34f646432
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-29 17:09:53 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignment/m3/Assignment-3/Solution.java
+++ cspp2-assignment/m3/Assignment-3/Solution.java
@@ -21,7 +21,7 @@ public final class Solution {
         Scanner s = new Scanner(System.in);      
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        System.out.println((int)gcd(n1, n2));
+        System.out.println((int) gcd(n1, n2));
     }
     /**
      * { function_description }.
