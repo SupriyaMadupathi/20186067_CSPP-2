9b0d99ba8461facc1864ec254e1c9fafd84b0f2e
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-29 17:44:50 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignment/m3/Assignment-2/Solution.java
+++ cspp2-assignment/m3/Assignment-2/Solution.java
@@ -24,7 +24,7 @@ public final class Solution {
       * @param      args  The arguments
       */
     public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
         for (int i = 1; i < n; i++) {
