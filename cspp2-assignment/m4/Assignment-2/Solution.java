4c4cdfa692ef8f79ecb5a7aade350fe916538dfe
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-30 23:30:42 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-2/Solution.java
+++ cspp2-assignment/m4/Assignment-2/Solution.java
@@ -20,32 +20,32 @@ public final class Solution {
         Scanner s = new Scanner(System.in);
         p = s.nextInt();
         q = s.nextInt();
-        int a[][] = new int[p][q];
+        int[][] a = new int[p][q];
         for (int i = 0; i < p; i++) {
                 for (int j = 0; j < q; j++) {
                     a[i][j] = s.nextInt();
                 }
-        for (int b = 0; b < p; b++) {
-                for (int j = 0; j < q; j++) {
-                   /* System.out.print(a[b][j]+" ");*/
-                }
-                /*System.out.println("");*/
-            }
-        }
+        // for (int b = 0; b < p; b++) {
+        //         for (int j = 0; j < q; j++) {
+        //             System.out.print(a[b][j]+" ");
+        //         }
+        //         /*System.out.println("");*/
+        //     }
+        // }
         m = s.nextInt();
         n = s.nextInt();
-        int b[][] = new int[m][n];
+        int[][] b = new int[m][n];
         for (int c = 0; c < m; c++) {
                 for (int j = 0; j < n; j++) {
                     b[c][j] = s.nextInt();
                 }
             }
-        for (int d = 0; d < m; d++) {
-                for (int j = 0; j < n; j++) {
-                }
-            }
+        // for (int d = 0; d < m; d++) {
+        //         for (int j = 0; j < n; j++) {
+        //         }
+        //     }
             if (p == m && q == n) {
-                int sum[][] = new int[m][n];
+                int[][] sum = new int[m][n];
                 for (int f = 0; f < p; f++) {
                     for (int j = 0; j < n; j++) {
                         sum[f][j] = a[f][j] + b[f][j];
@@ -64,5 +64,6 @@ public final class Solution {
         } else {
                 System.out.println("not possible");
             }
+}
 }
 }
