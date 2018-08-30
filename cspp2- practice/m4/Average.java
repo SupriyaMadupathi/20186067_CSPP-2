623218e8a96d468f698dde980b5a8a728407a908
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-30 14:30:16 +0530

    average of given array elements

--- cspp2- practice/m4/Average.java
+++ cspp2- practice/m4/Average.java
@@ -2,19 +2,27 @@ import java.util.Scanner;
 /**
  * @author     SupriyaMadupathi
  */
-public final class FirstLast6 {
+public final class Average {
     /**.
      * Constructs the object.
      */
-    private FirstLast6() {
+    private Average() {
     }
     /**
     * Main function to print.
     * @param args is the parameter for this function
     */
      public static void main(final String[] args) {
+        int n, sum = 0;
+        float avg;
         Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
+        n = s.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = s.nextInt();
+            sum = sum + arr[i];
+        }
+        avg = (float) sum / n;
+        System.out.println("Avarage is :" + avg);
+    }
+}
