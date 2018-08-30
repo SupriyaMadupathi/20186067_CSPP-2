0e2bbecb932e9ffaa1b2475c679a55fbd5914b47
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-30 15:00:35 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-1/Solution.java
+++ cspp2-assignment/m4/Assignment-1/Solution.java
@@ -26,8 +26,7 @@ public final class Solution {
             arr[i] = sc.nextInt();
         }
             int max = arr[0];
-            for (int i = 1; i < arr.length; i++ )
-            {
+            for (int i = 1; i < arr.length; i++ ) {
                 if (arr[i] > max) {
                     max = arr[i];
                 }
