f9cdd41dd6a101acce6d4aa35bb930dd793a1689
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-08-31 16:17:27 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-4/Solution.java
+++ cspp2-assignment/m4/Assignment-4/Solution.java
@@ -17,7 +17,7 @@ public class Solution
 	}
 	//Write reverseString function
 	static String reverseString(String s) {
-		String s1 = " ";
+		String s1 = "";
 		for (int i = s.length() - 1; i >= 0; i --) {
             s1 += s.charAt(i);
 		}			
