9c733caf7920a18c1296d99b445991aa3a1e717c
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-09-17 14:53:29 +0530

    "check_style_score -> 6 of 13 passed. style score: 0/1 "

--- cspp2-assignment/m20/Assignment-1/Solution.java
+++ cspp2-assignment/m20/Assignment-1/Solution.java
@@ -246,15 +246,16 @@ public final class Solution {
         // write your code here to read the questions from the console
         // tokenize the question line and create the question object
         // add the question objects to the quiz class
+		if (q == 0) {
+            System.out.println("Quiz does not have questions");
+            return;
+        }
         String a = scan.nextLine();
         String[] check = a.split(":");
         String[] checkQuestion = check[1].split(",");
         int help = Integer.parseInt(check[2]);
 
-		if (q == 0) {
-            System.out.println("Quiz does not have questions");
-            return;
-        }
+    
         if (check.length < 5) {
             System.out.println("Error! Malformed question");
             return;
